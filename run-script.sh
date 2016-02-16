8f5a7fd7dcfb19f4dac3cf641307e38ea92a622d
Maciej Bilas <maciej.bilas@gmail.com>
2016-02-16 17:12:15 +0100

    run-script.sh: add htop

--- run-script.sh
+++ run-script.sh
@@ -3,7 +3,7 @@
 echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 sudo apt-get update -y
-sudo apt-get install -y openjdk-8-jdk sbt vim redis-server screen
+sudo apt-get install -y openjdk-8-jdk sbt vim redis-server screen htop
 redis-server &
 git clone https://github.com/maciej/redis-benchmark.git
 cd redis-benchmark
